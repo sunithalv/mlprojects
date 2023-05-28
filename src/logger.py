a88a91dae98e12fcb7dc1519979d616b5b441683
Sunitha L V <sunithalv05@gmail.com>
2023-05-28 12:47:53 +0400

    Data ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import logging
 from datetime import datetime
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
